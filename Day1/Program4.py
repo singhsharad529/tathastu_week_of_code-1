df8869c372f3efc2b957c311333e4baa0620593e
Praveg Vashishtha <66157383+Praveg432@users.noreply.github.com>
2020-05-30 18:58:24 +0530

    Create Program4.py

---
+++ Day1/Program4.py
@@ -0,0 +1,7 @@
+cp=int(input("CP of material"))
+sp=int(input("SP of material"))
+p=sp-cp
+print("The Profit is:",p)
+print("If Profit is increase by 5%")
+sp2=p+5*p/100+cp
+print("Then the SP is:",sp2)
